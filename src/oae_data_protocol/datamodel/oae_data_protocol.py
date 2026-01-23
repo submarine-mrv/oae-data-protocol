0609a9f3244a72247b6d249a9ab848a27096fc21
Cory Levinson <cjlevinson@gmail.com>
2026-01-22 23:01:36 -0800

    update measurement/reported temp language

--- src/oae_data_protocol/datamodel/oae_data_protocol.py
+++ src/oae_data_protocol/datamodel/oae_data_protocol.py
@@ -1,5 +1,5 @@
 # Auto generated from oae_data_protocol.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-01-22T22:49:58
+# Generation date: 2026-01-22T23:00:38
 # Schema: OAEDataManagementProtocol
 #
 # id: OAEDataManagementProtocol
