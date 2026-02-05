70f393f5d2a4e17ecd2b6f20834ef2b39fca84d2
Cory Levinson <cjlevinson@gmail.com>
2026-02-05 14:24:19 -0800

    include rangeclass descendents in jsonschema

--- src/oae_data_protocol/datamodel/oae_data_protocol.py
+++ src/oae_data_protocol/datamodel/oae_data_protocol.py
@@ -1,5 +1,5 @@
 # Auto generated from oae_data_protocol.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-01-30T23:59:29
+# Generation date: 2026-02-05T14:23:46
 # Schema: OAEDataManagementProtocol
 #
 # id: OAEDataManagementProtocol
